151520cf62784d2032a9b37359eb466cf4fa3fd4
Julia Okhrymenko <Ohrimenko15@gmail.com>
2025-05-18 23:05:27 +0300

    'Solution'

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
@@ -112,7 +111,6 @@ class MovieViewSet(
             return MovieImageSerializer
 
         return MovieSerializer
-
 
     @action(
         methods=["POST"],
