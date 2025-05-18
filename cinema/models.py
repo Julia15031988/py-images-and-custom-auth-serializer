401b6766566faeb6332eb8122ea99c098f0ec8a8
Julia Okhrymenko <Ohrimenko15@gmail.com>
2025-05-18 23:11:51 +0300

    'Solution'

--- cinema/models.py
+++ cinema/models.py
@@ -40,9 +40,9 @@ class Actor(models.Model):
 
 
 def movie_image_path(instance: "Movie", filename: str) -> pathlib.Path:
-    filename = f"{slugify(instance.title)} - {uuid.uuid4()}" \
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}" \
                + pathlib.Path(filename).suffix
-    return pathlib.Path("upload/movies")/pathlib.Path(filename)
+    return pathlib.Path("upload/movies") / pathlib.Path(filename)
 
 
 class Movie(models.Model):
